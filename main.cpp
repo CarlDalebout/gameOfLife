439c7e7ff5b8abab699756ff535b3292b91fd72a
Carl <cadalebout1@couger.ciss.edu>
2024-07-31 16:32:57 -0500

    Initialization of gameOfLife

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <GL/freeglut.h>
+#include "../ciss380/a/mygllib/gl2d.h"
+
+
+
+void display()
+{
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    
+    glutSwapBuffers();
+}
+
+
+int main(int argc, char ** argv)
+{
+  glutInit(&argc, argv);
+  glutInitWindowPosition(300, 100);
+  glutInitWindowSize(400, 400);
+  glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA);
+  glutCreateWindow("OpenGL!!!");
+  glutDisplayFunc(display);
+  glutMainLoop();
+  return 0;
+}
